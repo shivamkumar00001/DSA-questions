0de46ef2783e314638f400e03e029e5776c4ca57
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-07-24 22:50:15 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (50.97%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -5,22 +5,25 @@ public:
            int m = mat.size();
            int n = mat[0].size();
            if(k%n == 0) return true;
-            vector<vector<int>> v(m, vector<int>(n, 0));
 
            for(int i=0;i<m;i++){
                
                if(i%2==1){
                   for(int j = 0 ;j<n ;j++){
-                      v[i][(j+k)%n] = mat[i][j];
+                      if(mat[i][(j+k)%n] != mat[i][j]){
+                      return false;
+                      }
                   }
                }
                else {
                    for(int j = 0 ;j<n ;j++){
-                      v[i][j] = mat[i][(j+k)%n];
+                      if(mat[i][j] != mat[i][(j+k)%n]){
+                        return false;
+                      }
                   }
                }
            }
 
-           return v == mat;
+           return true;
     }
 };
