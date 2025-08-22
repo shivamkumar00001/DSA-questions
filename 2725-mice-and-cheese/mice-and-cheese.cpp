d2c246acfcd569dc17433a250477812a2392c14c
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-08-22 13:02:12 +0530

    Time: 42 ms (47.38%) | Memory: 109.6 MB (83.44%) - LeetSync

--- 2725-mice-and-cheese/mice-and-cheese.cpp
+++ 2725-mice-and-cheese/mice-and-cheese.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     
-    typedef pair<int,int> p;
     int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
-        priority_queue<p, vector<p>, greater<p>>pq;
+        priority_queue<int, vector<int>, greater<int>>pq;
         int n = reward1.size();
         int maxi = 0;
         for(int i=0;i<n;i++){
-            pq.push({reward1[i]-reward2[i],i});
+            pq.push(reward1[i]-reward2[i]);
             if(pq.size()>k)pq.pop();
             maxi+=reward2[i];
         }
         while(pq.size()>0){
-            maxi+=reward1[pq.top().second];
-            maxi-=reward2[pq.top().second];
-            reward2[pq.top().second]=-1;
+            maxi+=pq.top();
             pq.pop();
         }
         return maxi;
