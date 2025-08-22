5000cc75368be26f6d17b1610911588fb2ff4166
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-08-22 12:59:24 +0530

    Time: 59 ms (28.51%) | Memory: 112.5 MB (51.78%) - LeetSync

--- 2725-mice-and-cheese/mice-and-cheese.cpp
+++ 2725-mice-and-cheese/mice-and-cheese.cpp
@@ -5,20 +5,18 @@ public:
     int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
         priority_queue<p, vector<p>, greater<p>>pq;
         int n = reward1.size();
+        int maxi = 0;
         for(int i=0;i<n;i++){
             pq.push({reward1[i]-reward2[i],i});
             if(pq.size()>k)pq.pop();
+            maxi+=reward2[i];
         }
-        int maxi = 0;
         while(pq.size()>0){
             maxi+=reward1[pq.top().second];
+            maxi-=reward2[pq.top().second];
             reward2[pq.top().second]=-1;
             pq.pop();
         }
-        for(int i=0;i<n;i++){
-            if(reward2[i]>0) maxi+=reward2[i];
-        }
-
         return maxi;
     }
 };
