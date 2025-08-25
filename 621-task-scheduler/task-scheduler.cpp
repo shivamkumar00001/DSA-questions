4511dfb65d3769640d111a2297db22cef265b53c
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-08-25 13:56:31 +0530

    Time: 4 ms (72.33%) | Memory: 38.2 MB (84.21%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -4,31 +4,47 @@ public:
         
         unordered_map<char,int>mp;
         priority_queue<int>pq;
+        int freq =0;
         for(auto x : tasks){
             mp[x]++;
+            freq = max (freq,mp[x]);
         }
-        for(auto x: mp){
-            pq.push(x.second);
+        int x =0;
+        for(auto y: mp){
+            if(y.second == freq)
+             x++;
         }
-        int time =0;
-        while(!pq.empty()){
-            vector<int> temp;
-            for(int i=1;i<=n+1;i++){
-                if(pq.size()>0){
-                int freq = pq.top();
-                pq.pop();
-                freq--;
-                temp.push_back(freq);
-                }
-            }
-            for(int i=0;i<temp.size();i++){
-                if(temp[i]>0)pq.push(temp[i]);
-            }
+        int y = tasks.size();
+        return max( (freq - 1)*(n+1) + x, y);
 
-            if(pq.size()>0) time+=(n+1);
-            else time += temp.size();
-        }
+        // first method using priority queue and greedy
 
-        return time;
+
+
+        // int time =0;
+        // while(!pq.empty()){
+        //     vector<int> temp;
+        //     for(int i=1;i<=n+1;i++){
+        //         if(pq.size()>0){
+        //         int freq = pq.top();
+        //         pq.pop();
+        //         freq--;
+        //         temp.push_back(freq);
+        //         }
+        //     }
+        //     for(int i=0;i<temp.size();i++){
+        //         if(temp[i]>0)pq.push(temp[i]);
+        //     }
+
+        //     if(pq.size()>0) time+=(n+1);
+        //     else time += temp.size();
+        // }
+
+        // return time;
+
+
+        // second method using only greedy and math;
+
+
     }
 };
