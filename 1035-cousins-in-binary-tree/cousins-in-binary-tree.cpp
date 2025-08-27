057cdeef36b31d6940d00acea13bb96e394e2cb7
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-08-27 17:42:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (91.17%) - LeetSync

---
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   int depthx =-1;
+   int depthy =-1;
+   TreeNode* parentx = nullptr;
+   TreeNode* parenty = nullptr;
+   void helper( TreeNode* root,TreeNode* parent, int x, int y,int d){
+     if(root == nullptr) return ;
+
+     if(root->val == x){
+        depthx = d;
+        parentx = parent;
+     }
+     else if(root->val == y){
+        depthy = d;
+        parenty = parent;
+     }
+     helper(root->left,root, x,y,d+1);
+     helper(root->right,root, x,y,d+1);
+   }
+    bool isCousins(TreeNode* root, int x, int y) {  
+        helper(root ,nullptr, x,y,0);
+        if(depthx == depthy ) return parentx!= parenty;
+        return false;
+    }
+};
