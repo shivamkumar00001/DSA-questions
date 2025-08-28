6aef49041feadc131741ea343a645374d8855887
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-08-28 18:17:06 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (93.99%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& dp) {
+
+        int m = dp.size();
+        int n = dp[0].size();
+
+        if(dp[0][0] == 1|| dp[m-1][n-1]==1) return 0;
+
+        dp[0][0] = 1;
+
+        for(int i =1;i<m;i++){
+            dp[i][0] = (dp[i][0] == 1? 0:dp[i-1][0] );
+        }
+
+        for(int i =1;i<n;i++){
+            dp[0][i] = (dp[0][i] == 1? 0:dp[0][i-1] );
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(dp[i][j]==1) dp[i][j] =0;
+                else dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+};
