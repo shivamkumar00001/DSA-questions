0bb8200be831b9c8f04b93dbd5de7f3e1013e807
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-09-18 20:02:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (14.22%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+   vector<vector<int>> ans;
+    void helper( int & k, int n, vector<int> res, int ind){
+        if(res.size() == k && n ==0){
+            ans.push_back(res);
+            return ;
+        }
+
+        if(res.size() == k) return;
+
+        for(int i=ind;i<=9;i++){
+            res.push_back(i);
+            helper(k,n-i,res,i+1);
+            res.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        
+        vector<int> res;
+        helper(k, n, res,1);
+        return ans;
+    }
+};
