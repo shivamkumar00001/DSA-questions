ae00f151d86df91aaa0c41cddcfd3dcd511ffcee
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-09-26 23:18:56 +0530

    Time: 3 ms (42.39%) | Memory: 10.2 MB (45.26%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-
+    vector<vector<int>> dp;
     int helper( vector<int> & nums, int i, int j){
 
         if(i== j) return nums[i];
-
-        return max(nums[i]-helper(nums,i+1,j), nums[j]-helper(nums, i, j-1));
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = max(nums[i]-helper(nums,i+1,j), nums[j]-helper(nums, i, j-1));
     }
     bool predictTheWinner(vector<int>& nums) {
         
         int i =0;
         int j = nums.size()-1;
+        dp.resize(j+2, vector<int>(j+2,-1));
         int val = helper(nums, i, j);
         if(val>=0) return true;
         return false;
