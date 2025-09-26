ef9fff2d31d9f4bcf8d3cbb7cb9ed82da56e4c9b
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-09-26 17:28:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (6.30%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,29 +1,55 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& dp) {
-
-        int m = dp.size();
-        int n = dp[0].size();
-
-        if(dp[0][0] == 1|| dp[m-1][n-1]==1) return 0;
-
-        dp[0][0] = 1;
-
-        for(int i =1;i<m;i++){
-            dp[i][0] = (dp[i][0] == 1? 0:dp[i-1][0] );
+    vector<vector<int>> dp;
+    vector<vector<int>> visited;
+    int helper ( vector<vector<int>>& grid , int i, int j ){
+        
+        if(i == grid.size()-1 && j == grid[0].size()-1){
+            return 1;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        int result =0;
+        if(i<grid.size()-1 && grid[i+1][j] ==0 && visited[i][j]==0){
+            visited[i][j] =1;
+            result += helper(grid, i+1, j);
+            visited[i][j] = 0;
         }
 
-        for(int i =1;i<n;i++){
-            dp[0][i] = (dp[0][i] == 1? 0:dp[0][i-1] );
+        if(j<grid[0].size()-1 && grid[i][j+1] ==0 && visited[i][j] ==0){
+            visited[i][j] =1;
+            result += helper(grid, i, j+1);
+            visited[i][j] =0;
         }
 
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(dp[i][j]==1) dp[i][j] =0;
-                else dp[i][j] = dp[i-1][j]+dp[i][j-1];
-            }
-        }
+        return dp[i][j] = result;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
 
-        return dp[m-1][n-1];
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if(grid[0][0] == 1|| grid[m-1][n-1]==1) return 0;
+
+        // dp[0][0] = 1;
+
+        // for(int i =1;i<m;i++){
+        //     dp[i][0] = (dp[i][0] == 1? 0:dp[i-1][0] );
+        // }
+
+        // for(int i =1;i<n;i++){
+        //     dp[0][i] = (dp[0][i] == 1? 0:dp[0][i-1] );
+        // }
+
+        // for(int i=1;i<m;i++){
+        //     for(int j=1;j<n;j++){
+        //         if(dp[i][j]==1) dp[i][j] =0;
+        //         else dp[i][j] = dp[i-1][j]+dp[i][j-1];
+        //     }
+        // }
+
+        // return dp[m-1][n-1];
+        dp.resize(m, vector<int>(n,-1));
+        visited.resize(m, vector<int>(n,0));
+        return helper(grid, 0, 0);
     }
 };
