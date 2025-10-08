c44eec25ca30a8650f847de153520a52f6edd1bf
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-10-08 18:16:07 +0530

    Time: 108 ms (8.09%) | Memory: 163.8 MB (5.12%) - LeetSync

---
+++ 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    vector<vector<int>>dp;
+    int helper( vector<int>& prices, int ind, int reward){
+
+        if(ind  == prices.size()) return 0;
+        if(dp[ind][reward]!= -1) return dp[ind][reward];
+        int ans = INT_MAX;
+        if(reward>0){
+            ans = min(ans, helper(prices, ind+1, reward-1));
+        }
+        ans = min(ans, prices[ind]+ helper(prices,ind+1,ind+1));
+
+        return dp[ind][reward]= ans;
+    }
+    int minimumCoins(vector<int>& prices) {
+        
+        dp.resize(prices.size()+5,vector<int>(prices.size()*2+5,-1 ));
+        return helper( prices, 0, 0);
+    }
+};
