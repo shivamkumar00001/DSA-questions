733d6f318f29ec37596a53363c52f3c0c054b467
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-10-21 23:42:45 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (44.49%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -9,22 +9,53 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int maxDia =0;
+//     int maxh(TreeNode *root){
+//         if(!root) return 0;
+//         return max(1+maxh(root->left), 1+maxh(root->right));
+//     }
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         int left =0;
+//         int right =0;
+//         if(!root) return 0;
+//         int dia = maxh(root->left)+maxh(root->right);
+//         return max({dia,diameterOfBinaryTree(root->left),
+//         diameterOfBinaryTree(root->right)});
+        
+//     }
+// };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int maxDia =0;
-    int maxh(TreeNode *root){
-        if(!root) return 0;
+    
+    int maxdia =0;
 
-        return max(1+maxh(root->left), 1+maxh(root->right));
+    int height(TreeNode * root){
+        if (!root) return 0;
+        
+        int left = height(root->left);
+        int right = height(root->right);
+
+        maxdia = max(maxdia, left+right);
+
+        return 1 + max(left,right);
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        int left =0;
-        int right =0;
-        if(!root) return 0;
-        int dia = maxh(root->left)+maxh(root->right);
         
-        return max({dia,diameterOfBinaryTree(root->left),
-        diameterOfBinaryTree(root->right)});
-        
+        height(root);
+        return maxdia;
     }
 };
