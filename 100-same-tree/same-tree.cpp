4be1ccdd6a0dd4ccf416bf2b43e51f2fa35462af
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-10-22 23:24:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (14.70%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -15,8 +15,6 @@ public:
         if(!p && !q) return true;
         if(!p || !q) return false;
         if(p->val != q->val) return false;
-        if(isSameTree(p->left,q->left) && isSameTree(p->right,q->right)) return true;
-
-        return false;
+        return (isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
     }
 };
