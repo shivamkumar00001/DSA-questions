1c62ae6d59de6a63ef773b2a5d7be3852f91dd41
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-11-03 16:04:22 +0530

    Time: 67 ms (57.77%) | Memory: 14.5 MB (48.38%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int lcs ( vector<int>& nums, vector<int>& v, int i, int j){
-        if(i == nums.size() || j == v.size()) return 0;
-        if(dp[i][j]!= -1) return dp[i][j];
-        if(nums[i] == v[j]){
-            return dp[i][j] = 1 + lcs(nums, v, i+1, j+1);
-        }
-        else return dp[i][j] = max(lcs(nums, v,i+1,j), lcs(nums,v,i,j+1));
-    }
+    
     int lengthOfLIS(vector<int>& nums) {
-        
-        set<int>s;
+       
+       int n = nums.size();
 
-        for(int i=0;i<nums.size();i++){
-            s.insert(nums[i]);
+       vector<int>dp(n+1,1);
+       int maxval = 1;
+       for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(nums[j]<nums[i]){
+                dp[i] = max(dp[i],dp[j]+1);
+                maxval = max(maxval,dp[i]);
+            }
         }
-        vector<int>v;
-        for(auto x : s){
-            cout<<x<<" ";
-            v.push_back(x);
-        }
-        dp.resize(nums.size()+1, vector<int>(v.size()+1,-1));
-        return lcs(nums, v, 0 , 0);
-        
+       }
+       return maxval;
 
     }
 };
