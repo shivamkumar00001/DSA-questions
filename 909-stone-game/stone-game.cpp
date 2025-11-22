7e8eec14329a130cfe6806d36a594434031d9bba
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-11-22 19:09:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (87.32%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int helper(vector<int>&piles, int i, int j){
+    // vector<vector<int>>dp;
+    // int helper(vector<int>&piles, int i, int j){
 
-        if(i==j) return piles[i];
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = max(piles[i]-helper(piles,i+1,j), piles[j]-helper(piles, i, j-1));
-    }
+    //     if(i==j) return piles[i];
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     return dp[i][j] = max(piles[i]-helper(piles,i+1,j), piles[j]-helper(piles, i, j-1));
+    // }
     bool stoneGame(vector<int>& piles) {
-        dp.resize(piles.size()+5, vector<int>(piles.size()+5,-1));
-        int val = helper(piles,0,piles.size()-1);
-        return val>0;
+        // dp.resize(piles.size()+5, vector<int>( piles.size()+5,-1));
+        // int val = helper(piles,0,piles.size()-1);
+        // return val>0;
+        return true;
     }
 };
