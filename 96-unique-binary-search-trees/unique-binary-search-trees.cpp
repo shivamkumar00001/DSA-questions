5afed6b43134da134a1ca2fa44df59f47632e149
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2025-11-26 12:32:39 +0530

    Time: 2314 ms (5.04%) | Memory: 7.9 MB (61.52%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    vector<int> dp;
-    int f( int n){
+  
+    int f( int i, int n){
 
-        if(n ==0 || n== 1) return 1;
-        if(dp[n] != -1) return dp[n];
-        int sum =0;
-        for(int k =1;k<=n ;k++){
-            sum+= f(k-1)* f(n-k);
+        if(i>=n) return 1;
+      
+        long long  sum =0;
+        for(int k =i;k<=n ;k++){
+            sum+= f(i,k-1)* f(k+1,n);
         }
-        return dp[n] = sum;
+        return  sum;
     }
     int numTrees(int n) {
-      dp.resize(n+1,-1);
-      return  f( n );
+      return  f( 1,n );
     }
 };
