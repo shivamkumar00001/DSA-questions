affa356accd26e75a297c60e3851366ccf757a7b
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2026-01-29 19:23:45 +0530

    Time: 764 ms (5.02%) | Memory: 100.9 MB (5.02%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,53 +1,50 @@
 class Solution {
 public:
-    int n, m;
-    vector<vector<int>> vis;
-
-    void dfs(vector<vector<int>>& heights, int i, int j, vector<int>& a) {
-        // boundary → Pacific
-        if (i < 0 || j < 0) {
+ vector<vector<int>> vis;
+    void dfs(vector<vector<int>>& heights, int i, int j, vector<int>& a){
+        if( i < 0 || j < 0 ){
             a[0] = 0;
             return;
         }
-        // boundary → Atlantic
-        if (i >= n || j >= m) {
-            a[1] = 1;
+        if(i>=heights.size() || j == heights[0].size()){
+            a[1] =1;
             return;
         }
 
         if (vis[i][j]) return;
         vis[i][j] = 1;
 
-        if (i == 0 || heights[i][j] >= heights[i - 1][j])
-            dfs(heights, i - 1, j, a);
-
-        if (j == 0 || heights[i][j] >= heights[i][j - 1])
-            dfs(heights, i, j - 1, a);
-
-        if (i == n - 1 || heights[i][j] >= heights[i + 1][j])
-            dfs(heights, i + 1, j, a);
-
-        if (j == m - 1 || heights[i][j] >= heights[i][j + 1])
-            dfs(heights, i, j + 1, a);
+        if( i== 0 || heights[i][j]>= heights[i-1][j]){
+            dfs(heights, i-1, j,a);
+        }
+        if( j== 0 || heights[i][j]>= heights[i][j-1]){
+            dfs(heights, i, j-1,a);
+        }
+        if( i== heights.size()-1 || heights[i][j]>= heights[i+1][j]){
+            dfs(heights, i+1, j,a);
+        }
+        if( j== heights[0].size()-1 || heights[i][j]>= heights[i][j+1]){
+            dfs(heights, i, j+1,a);
+        }
+        return;
     }
-
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        n = heights.size();
-        m = heights[0].size();
-        vector<vector<int>> result;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+         vector<vector<int>>result;
+         int n = heights.size();
+         int m = heights[0].size();
+         for(int i = 0 ; i< heights.size(); i++){
+            for(int j =0; j< heights[0].size(); j++){
                 vis.assign(n, vector<int>(m, 0));
-                vector<int> a = {-1, -1};
-
+                vector<int>a;
+                a.push_back(-1);
+                a.push_back(-1);
                 dfs(heights, i, j, a);
-
-                if (a[0] == 0 && a[1] == 1) {
-                    result.push_back({i, j});
+                if(a[0] == 0 && a[1] == 1){
+                    result.push_back({i,j});
                 }
             }
-        }
-        return result;
+         }
+
+         return result;
     }
 };
