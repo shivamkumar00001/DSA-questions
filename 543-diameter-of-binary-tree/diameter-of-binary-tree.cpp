25ce8439e18d9aa91b85549458f6d013c0646b41
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2026-02-05 18:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (72.15%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -9,53 +9,22 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     int maxDia =0;
-//     int maxh(TreeNode *root){
-//         if(!root) return 0;
-//         return max(1+maxh(root->left), 1+maxh(root->right));
-//     }
-//     int diameterOfBinaryTree(TreeNode* root) {
-//         int left =0;
-//         int right =0;
-//         if(!root) return 0;
-//         int dia = maxh(root->left)+maxh(root->right);
-//         return max({dia,diameterOfBinaryTree(root->left),
-//         diameterOfBinaryTree(root->right)});
-        
-//     }
-// };
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    
-    int maxdia =0;
+    int maxi =INT_MIN;
 
-    int height(TreeNode * root){
-        if (!root) return 0;
-        
-        int left = height(root->left);
-        int right = height(root->right);
+    int helper( TreeNode* root){
+        if( root == nullptr) return 0;
 
-        maxdia = max(maxdia, left+right);
+        int left = helper(root ->left);
+        int right = helper(root ->right);
+
+        maxi = max( maxi , (left+right));
 
         return 1 + max(left,right);
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        
-        height(root);
-        return maxdia;
+        int val = helper(root);
+        return maxi;
     }
 };
