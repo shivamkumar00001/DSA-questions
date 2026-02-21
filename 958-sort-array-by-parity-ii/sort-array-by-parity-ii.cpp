30b8315b639a23736bb41b3b1714bf2c98bd4ead
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2026-02-21 17:25:49 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (45.04%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -8,14 +8,25 @@ public:
         while(i<n && j<n){
             if(nums[i]%2> nums[j]%2){
                 swap(nums[i],nums[j]);
+                i+=2;
+                j+=2;
             }
-            if(nums[i]%2 == 0){
+            else if(nums[i]%2 == 0){
                 i+=2;
             }
-            if(nums[j] %2 != 0){
+            else if(nums[j] %2 != 0){
                 j+=2;
             }
         }
         return nums;
     }
 };
+
+
+
+
+
+
+
+
+
