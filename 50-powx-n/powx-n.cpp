02b1f0650dac3c4ab30d7e50d24c6be5cb822e47
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2026-02-22 12:09:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.10%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -3,7 +3,7 @@ public:
     double helper(double x, long long  n){
         if(n == 1) return x;
         if(n == 0) return 1;
-        double ans = myPow(x,n/2);;
+        double ans = helper(x,n/2);;
         if(n%2 == 0){
            return ans*ans;}
         else {
