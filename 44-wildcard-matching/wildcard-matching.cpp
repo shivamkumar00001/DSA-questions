e150cd6e0bbd15768474b4735fcf64742fdd4846
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2026-03-30 12:31:13 +0530

    Time: 147 ms (7.90%) | Memory: 55.3 MB (5.84%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
    
-    vector<vector<int>> dp;
-
-    bool helper(string &s, string &p, int i, int j){
+   vector<vector<int>>dp;
+    bool helper( string& s, string& p, int i, int j){
          
-        if(i == s.size() && j == p.size()) return true;
-
-        if(j == p.size()) return false;
-
-        // 🔥 Important fix
-        if(i == s.size()){
-            for(int k = j; k < p.size(); k++){
-                if(p[k] != '*') return false;
-            }
+        if( i == s.size() && j == p.size()){
             return true;
         }
 
+        if(j == p.size() ) return false;
+        
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(s[i] == p[j] || p[j] == '?'){
-            return dp[i][j] = helper(s, p, i+1, j+1);
+        if(i<s.size() && (s[i] == p[j] || p[j] == '?')){
+            return dp[i][j] = (helper(s,p,i+1,j+1));
         }
 
         if(p[j] == '*'){
-            return dp[i][j] = (helper(s, p, i+1, j) || helper(s, p, i, j+1));
+           return dp[i][j] = ((i<s.size()&&helper(s,p,i+1,j))|| helper(s,p,i,j+1));
         }
-
         return dp[i][j] = false;
     }
-
     bool isMatch(string s, string p) {  
-        dp.assign(s.size()+1, vector<int>(p.size()+1, -1));
-        return helper(s, p, 0, 0);
+        dp.resize(s.size()+1,vector<int>(p.size()+1,-1));
+        return  helper(s, p, 0, 0);
     }
 };
