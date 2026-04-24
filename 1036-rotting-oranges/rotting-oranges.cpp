ab121104169accfe708077ed260b4b6f11fda621
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2026-04-24 11:13:05 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (86.00%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,49 +1,49 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-         
-         queue<pair<int,int>>q;
-         int count =0;
-         for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j] == 2)q.push({i,j});
-                if(grid[i][j] == 1)count++;
+        
+        queue<pair<int,int>> q;
+        int count = 0;
+        for(int i=0 ; i<grid.size() ; i++){
+            for(int j=0 ; j<grid[0].size() ; j++){
+                if(grid[i][j] == 2) {
+                    q.push({i,j});
+                }
+                if(grid[i][j] == 1){
+                    count++;
+                }
             }
-         }
-         
-         int total =0;
-         int ans=0;
-         while(q.size()>0){
+        }
+        
+        vector<pair<int,int>>dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        int time = -1;
+        int ct =0;
+        while(q.size()>0){
+             
              int s = q.size();
+             time++;
              for(int i=0;i<s;i++){
                  
-                 auto [a,b] = q.front();
+                 int a = q.front().first;
+                 int b = q.front().second;
                  q.pop();
-                 if(a+1<grid.size() && grid[a+1][b] ==1){
-                    grid[a+1][b] =2;
-                    q.push({a+1,b});
-                    total++;
+                 
+                 for(int d=0;d<dirs.size();d++){
+                     int x = dirs[d].first;
+                     int y = dirs[d].second;
+
+                     if( a+x<grid.size() && a+x >=0 && b+y>=0 && b+y<grid[0].size() && grid[a+x][b+y] == 1){
+                         ct++;
+                         grid[a+x][b+y] =2;
+                         q.push({a+x,b+y});
+                     }
                  }
-                 if(a-1>=0 && grid[a-1][b] ==1){
-                    grid[a-1][b] =2;
-                    q.push({a-1,b});
-                    total++;
-                 }
-                 if(b+1<grid[0].size() && grid[a][b+1] ==1){
-                    grid[a][b+1] =2;
-                    q.push({a,b+1});
-                    total++;
-                 }
-                 if(b-1>=0 && grid[a][b-1] ==1){
-                    grid[a][b-1] =2;
-                    q.push({a,b-1});
-                    total++;
-                 }
+                 
              }
-             ans++;
-         }
-         if(count == 0 && total ==0) return 0;
-         if(count == total) return ans-1;
-         return -1;
+        }
+        if(count ==0) return 0;
+        if(ct == count) return time;
+        return -1;
+ 
     }
 };
