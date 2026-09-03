2af3310d1fd003f27aee35d1fc9451ecd5133eb0
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2026-09-03 23:23:33 +0530

    Time: 4 ms (15.04%) | Memory: 16.6 MB (16.14%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     
     vector<vector<int>>dp;
-    long long helper( vector<vector<int>>&grid, int i, int j){
-         
-        if( i == grid.size()-1 && j == grid[0].size()-1) return grid[i][j];
-        
+    int helper(vector<vector<int>>& grid,int i, int j){
+        int m = grid.size();
+        int n = grid[0].size();
+        if( i == m-1 && j == n-1) return grid[i][j];
         if(dp[i][j] != -1) return dp[i][j];
-        long long ans1=INT_MAX;
-        long long ans2=INT_MAX;
-       if(i<grid.size()-1)
-        ans1 = helper(grid,i+1,j);
-       if(j<grid[0].size()-1)
-        ans2 = helper(grid,i,j+1);
+        long ans1 = INT_MAX;
+        long ans2 = INT_MAX;
+        if(i<m-1){
+            ans1 = helper(grid,i+1,j);
+        }
+        if(j<n-1){
+            ans2 = helper(grid,i,j+1);
+        }
 
-        return dp[i][j] = grid[i][j] +min(ans1,ans2); 
+        return dp[i][j] =  grid[i][j]+min(ans1,ans2);
+
     }
     int minPathSum(vector<vector<int>>& grid) {
-       dp.resize(grid.size()+5,vector<int>(grid[0].size()+5,-1));
-       return  helper( grid , 0,0);
+        dp.resize(grid.size(),vector<int>(grid[0].size(),-1));
+        return helper(grid, 0,0);
+
     }
 };
