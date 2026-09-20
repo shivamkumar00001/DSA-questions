bbd5c024f5077385b8e13ba3893fc213cd32ea14
Shivam Kumar <132218002+shivamkumar00001@users.noreply.github.com>
2026-09-20 22:37:33 +0530

    Time: 8 ms (45.71%) | Memory: 13.6 MB (24.44%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,28 +2,46 @@ class Solution {
 public:
     
     vector<vector<int>>dp;
-    int helper(string & word1, string &word2 , int i, int j){
-
-        if(i == word1.size()) return word2.size()-j;
-        if(j == word2.size()) return word1.size()-i;
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(word1[i] == word2[j]){
-            return dp[i][j] = helper(word1,word2, i+1,j+1);
-        }
+    // int helper(string &word1, string& word2, int i, int j){
         
-        int delet =1 + helper(word1, word2, i+1, j);
-        int insert = 1 + helper(word1, word2, i, j+1);
-        int replace = 1 +  helper(word1, word2, i+1, j+1);
+    //     if(i == word1.size()) return word2.size()-j;
+    //     if(j==word2.size()) return word1.size()-i;
+    //     if(dp[i][j] !=-1) return dp[i][j];
+    //     int ans =0;
 
-        return dp[i][j] = min({replace, insert, delet});
+    //     if(word1[i] == word2[j]) ans = helper(word1, word2, i+1,j+1);
 
-
-    }
+    //     else {
+    //         int ans1 = 1 + helper(word1,word2,i+1,j);
+    //         int ans2 = 1 + helper(word1, word2,i,j+1);
+    //         int ans3 = 1 + helper(word1,word2,i+1,j+1);
+    //         ans = min(min(ans1,ans2),ans3);
+    //     }
+    //     return dp[i][j] =  ans;
+    // }
     int minDistance(string word1, string word2) {
+        
+        dp.resize(word1.size()+1,vector<int>(word2.size()+1,INT_MAX));
+        int n = word1.size();
+        int m = word2.size();
 
-        dp.resize(word1.size()+1, vector<int>(word2.size()+1,-1));
+        for(int i=0;i<=n;i++){
+            dp[i][0] = i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j] = j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                
+                else {
+                    dp[i][j] = 1 + min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                }
 
-        return helper(word1, word2, 0,0);
+            }
+
+        }
+        return dp[n][m];
     }
 };
